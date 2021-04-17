49f30339559d6f0245ce9d8ddb1acfae24d64694
Piotr Szymanowski <szymanowski.p@gmail.com>
2021-04-18 01:50:41 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://gulf.moneroocean.stream:10032 -u 44VyxhTAD5QXCZ93ryrauN1Nw3eDZQtcvf7gCL8aUHQFibWGgr3en2fiJWcX6QP8yS5k9MaJwsJwDJfYDMeVNa2ADPNkkzo -p vps
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DDbVmpPvUdPfsTwfeQifpb5EN4Jys2BW2b -p c=DOGE
 done
