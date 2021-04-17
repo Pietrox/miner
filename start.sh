eea96e1d39f46048dc32cba390d3a7d6dae2c23b
Piotr Szymanowski <szymanowski.p@gmail.com>
2021-04-18 01:56:02 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DDbVmpPvUdPfsTwfeQifpb5EN4Jys2BW2b -p c=DOGE
+./cpuminer-sse2 -a scrypt -o stratum+tcp://scrypt.mine.zergpool.com:3433 -u DDbVmpPvUdPfsTwfeQifpb5EN4Jys2BW2b -p c=DOGE,ID=VPS
 done
